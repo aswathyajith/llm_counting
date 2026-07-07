a715f5f6b66d4c282391718a2aacbc40bf286abf
aswathyajith <aswathy@uchicago.edu>
2026-07-07 16:00:53 -0500

    Gradated Ablation Example Generation

--- src/data/context_ablation.py
+++ src/data/context_ablation.py
@@ -2,26 +2,51 @@ import random
 import pandas as pd
 
 
+def create_cipher_mapping() -> dict:
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    shuffled = list(alphabet)
+    random.seed(999)
+    random.shuffle(shuffled)
+    mapping = {}
+    for i, letter in enumerate(alphabet):
+        mapping[letter] = shuffled[i]
+        mapping[letter.upper()] = shuffled[i].upper()
+    return mapping
+
+
+def map_words_in_text_with_prob(text: str, replace_prob: float, mapping: dict) -> str:
+    map_word_cipher = False
+    new_text = ""
+    for i in range(len(text)):
+        # if it is the first character or
+        # previous character is a whitespace
+        # then a new word is starting
+        # print(i)
+        if (i == 0) or (text[i - 1].isspace()):
+            random_toss = random.random()
+            if random_toss <= replace_prob:
+                map_word_cipher = True
+            else:
+                map_word_cipher = False
+        if text[i] in mapping and map_word_cipher:
+            new_text += mapping[text[i]]
+        else:
+            new_text += text[i]
+    return new_text
+
+
 def context_ablation(
     data: pd.DataFrame,
     original_column: str = "noun_context",
     new_column: str = "ablated_cipher_context",
     type: str = "cipher",
+    cipher_swap_rate: float = 1.0,
 ) -> pd.DataFrame:
     if type == "cipher":
-        random.seed(999)
-        alphabet = "abcdefghijklmnopqrstuvwxyz"
-        shuffled = list(alphabet)
-        random.shuffle(shuffled)
-        mapping = {}
-
-        def get_mapped_string(string):
-            return "".join(
-                [mapping[char] if char in mapping else char for char in string]
-            )
-
-        for i, letter in enumerate(alphabet):
-            mapping[letter] = shuffled[i]
-            mapping[letter.upper()] = shuffled[i].upper()
-        data[new_column] = data[original_column].apply(get_mapped_string)
+        mapping = create_cipher_mapping()
+        data[new_column] = data[original_column].apply(
+            lambda x: map_words_in_text_with_prob(x, cipher_swap_rate, mapping)
+        )
+    elif type == "reverse":
+        data[new_column] = data[original_column].apply(lambda x: x[::-1])
     return data
